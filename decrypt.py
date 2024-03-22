0a4184c907327ef1d1356397d1950c999c2edf62
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2024-03-22 21:56:55 +0800

    add functions verify() and decrypt() to decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -5,30 +5,15 @@ from Crypto.Signature import pkcs1_15
 from Crypto.Hash import SHA256
 import json
 
-# import keys
-pk = RSA.importKey(open("pk.pem").read())
-sk = RSA.importKey(open("sk.pem").read(), passphrase=b"password")
 
-# read message and store as dictionary
-with open("msg.json", "r") as f:
-    hex_message = json.load(f)
+def verify(pk, text, signature):
+    # Hash is mandatory for this implementation of PKCS #1: https://pycryptodome.readthedocs.io/en/latest/src/signature/pkcs1_v1_5.html
+    hashed_text = SHA256.new(text)
+    verifier = pkcs1_15.new(pk)
+    verifier.verify(hashed_text, signature)
 
-f.close()
 
-# convert ciphertext and signature back to bytes:
-cipher_text = bytes.fromhex(hex_message["ciphertext"])
-signature = bytes.fromhex(hex_message["signature"])
-
-# recreating the hashed file
-hashed_cipher_text = SHA256.new(cipher_text)
-
-# verifying and decrypting the file
-verifier = pkcs1_15.new(pk)
-try:
-    verifier.verify(hashed_cipher_text, signature)
+def decrypt(sk, ciphertext):
     cipher = PKCS1_OAEP.new(sk)
-    text = cipher.decrypt(cipher_text)
-    print(text.decode())
-except:
-    print("invalid message")
-
+    text = cipher.decrypt(ciphertext)
+    return text
