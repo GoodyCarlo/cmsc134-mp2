829050cd3bfe98c315f8736d64ec02c0b1bf147c
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2024-03-22 21:43:12 +0800

    add functions encrypt() and sign() to encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -6,26 +6,16 @@ from Crypto.Hash import SHA256
 
 import json
 
-# loading keys
-sk = RSA.importKey(open("sk.pem").read(), passphrase=b"password")
-pk = RSA.importKey(open("pk.pem").read())
 
-# creating and encrypting message
-message = b"You can attack now!"
-cipher = PKCS1_OAEP.new(pk)
-cipher_text = cipher.encrypt(message)
+def encrypt(pk, plaintext):
+    cipher = PKCS1_OAEP.new(pk)
+    ciphertext = cipher.encrypt(plaintext)
+    return ciphertext
 
 
-# signing the message
-hashed_cipher_text = SHA256.new(cipher_text)
-signer = pkcs1_15.new(sk)
-signature = signer.sign(hashed_cipher_text)
+def sign(sk, text):
+    hashed_text = SHA256.new(text)
+    signer = pkcs1_15.new(sk)
+    signature = signer.sign(hashed_text)
 
-# encrypting message to a file
-message = {"ciphertext": cipher_text.hex(), "signature": signature.hex()}
-
-with open("msg.json", "w") as f:
-    f.write(json.dumps(message, indent=4))
-
-f.close()
-
+    return signature
