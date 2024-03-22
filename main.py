829d761773b2e32ec05d648c6cd45ed43ce43069
jav <joseangelo@bposeats.com>
2024-03-23 01:49:55 +0800

    Added argsparsing
    - running main.py should require an argument now, specifically a string
    - users can use the `-c` flag to create a new key; no more keys being created for each run

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
+# Args parsing
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("message", type=str, help="Enter a message to be encrypted/decrypted")
+parser.add_argument("-c", "--createkey", action='store_true', help="Creates a new key")
+args = parser.parse_args()
+
+message = args.message
+
+# Main
 import create_key, receiver, sender
-
-message = "Insert message here"
-
-create_key.main()
+if args.createkey:
+    create_key.main()
 sender.main(message)
 receiver.main()
