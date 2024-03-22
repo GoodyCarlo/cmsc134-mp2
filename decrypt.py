0df074853c8b0b8ad8ed37c84f8d5a3ee286f943
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2024-03-22 22:01:33 +0800

    verify & decrypt message when calling decrypt.py as main

--- decrypt.py
+++ decrypt.py
@@ -17,3 +17,21 @@ def decrypt(sk, ciphertext):
     cipher = PKCS1_OAEP.new(sk)
     text = cipher.decrypt(ciphertext)
     return text
+
+
+if __name__ == "__main__":
+    # import keys
+    verify_public_key = RSA.importKey(open("pk.pem").read())
+    decrypt_private_key = RSA.importKey(open("sk.pem").read(), passphrase="password")
+
+    # read message and store from dictionary
+    with open("msg.json", "r") as f:
+        hex_message = json.load(f)
+
+        # convert ciphertext and signature back to bitstring
+        ciphertext = bytes.fromhex(hex_message["ciphertext"])
+        signature = bytes.fromhex(hex_message["signature"])
+
+    verify(verify_public_key, ciphertext, signature)
+    plaintext = decrypt(decrypt_private_key, ciphertext)
+    print(plaintext.decode())
