1f4a9205efdd1ef2e32337f9d02aa8bf44e2c1e3
Jethro Modequillo <jethro.modequillo@mensaphilippines.org>
2024-03-22 21:17:13 +0800

    add generate_keypair() in create_key.py

--- create_key.py
+++ create_key.py
@@ -4,30 +4,21 @@ from Crypto.Cipher import PKCS1_OAEP
 
 from Crypto.PublicKey import RSA
 
-# generate and export public and private key
-mykey = RSA.generate(3072)
 
-pwd = b"password"
+def generate_keypair():
+    mykey = RSA.generate(
+        3072
+    )  # TODO: Consider using a 2048-bit key just cuz that's what everyone does
 
-with open("sk.pem", "wb") as f:
+    pwd = "password"
 
-    data = mykey.export_key(
+    public = mykey.public_key().export_key()
+
+    private = mykey.export_key(
         passphrase=pwd,
         pkcs=8,
         protection="PBKDF2WithHMAC-SHA512AndAES256-CBC",
         prot_params={"iteration_count": 131072},
     )
 
-    f.write(data)
-
-f.close()
-
-
-with open("pk.pem", "wb") as f:
-
-    data = mykey.public_key().export_key()
-
-    f.write(data)
-
-f.close()
-
+    return public, private
