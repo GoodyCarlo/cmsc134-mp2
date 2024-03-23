f05fc5d803f90574bdb5a48d5ddb0bd206c25a00
jav <joseangelo@bposeats.com>
2024-03-23 11:39:20 +0800

    added argsparse for `receiver.py`
    - main also has default values for parameters

--- receiver.py
+++ receiver.py
@@ -1,3 +1,4 @@
+import pathlib
 from Crypto.Cipher import PKCS1_OAEP
 from Crypto.PublicKey import RSA
 from Crypto.Signature import pkcs1_15
@@ -18,18 +19,24 @@ def decrypt(sk, ciphertext):
     return text
 
 
-def main():
+def main(
+        spkPATH=pathlib.WindowsPath('keys/signing_pk.pem'),
+        eskPATH=pathlib.WindowsPath('keys/encryption_sk.pem'),
+        messagePath=pathlib.WindowsPath('msg.json'),
+        pwd="password"
+    ):
+
     # import keys
     verify_public_key = RSA.importKey(
-        open("keys/signing_pk.pem").read(),
+        open(spkPATH).read(),
     )
     decrypt_private_key = RSA.importKey(
-        open("keys/encryption_sk.pem").read(),
-        passphrase="password",
+        open(eskPATH).read(),
+        passphrase=pwd,
     )
 
     # read message and store from dictionary
-    with open("msg.json", "r") as f:
+    with open(messagePath, "r") as f:
         hex_message = json.load(f)
 
         # convert ciphertext and signature back to bitstring
@@ -48,4 +55,13 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    from args import receiverParser
+
+    args = receiverParser.parse_args()
+
+    main(
+        args.signingpk,
+        args.encryptionsk,
+        args.messagepath,
+        args.pwd
+    )
